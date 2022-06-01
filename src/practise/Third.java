77b952d0d5a38c75570d1aec4a121fdfc2a0bfdf
Zac-Jewitt <zacjewitt22@gmail.com>
2022-06-01 17:47:05 +1000

    Changes

--- src/practise/Third.java
+++ src/practise/Third.java
@@ -1,7 +1,25 @@
 package practise;
 
+import strings.CharactersSame;
+import strings.CheckAnagrams;
+
 public class Third {
     public static void main(String[] args) {
+        CheckAnagrams checkAnagrams = new CheckAnagrams();
+
+        System.out.println("Using CheckAnagrams");
+        System.out.println("water & hello = " +
+                checkAnagrams.isAnagrams("water", "hello"));
+        System.out.println("race & care = " +
+                checkAnagrams.isAnagrams("race", "care"));
+
+        CharactersSame charactersSame = new CharactersSame();
+
+        System.out.println("\nUsing CharactersSame");
+        System.out.println("ddddwwww = " +
+                charactersSame.isAllCharactersSame("ddddwwww"));
+        System.out.println("qqqqqqqq = " +
+                charactersSame.isAllCharactersSame("qqqqqqqq"));
 
     }
 }
