7d4ee97af2780832e5fbbc76127304e630eceed2
Zac-Jewitt <zacjewitt22@gmail.com>
2022-06-01 15:09:08 +1000

    Changes

---
+++ src/practise/Fourth.java
@@ -0,0 +1,14 @@
+package practise;
+
+public class Fourth {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y =  Integer.parseInt(args[1]);
+        int z = Integer.parseInt(args[2]);
+
+        int sum = x + y + z;
+
+        System.out.println(sum);
+
+    }
+}
